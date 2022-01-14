7bf18f6083a78214a5dafa3324e3b860454fff6a
vicente <vicente.real02@gmail.com>
2022-01-14 13:33:16 +0100

    el mesi

--- db.js
+++ db.js
@@ -2,12 +2,18 @@ const mongoose = require("mongoose")
 
 const mongoDB = process.env.MONGODB_URI
 
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+const dbConnection = async () =>{
+    
+    await mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+}
 
-const db = mongoose.connection
 
-db.on('error',console.error.bind(console, 'Fallo conexion BD!!'))
-db.once('open', () =>{
-    console.log("Conectado")
-})
+dbConnection()
+
+// const db = mongoose.connection
+
+// db.on('error',console.error.bind(console, 'Fallo conexion BD!!'))
+// db.once('open', () =>{
+//     console.log("Conectado")
+// })
 //gfhfgh
